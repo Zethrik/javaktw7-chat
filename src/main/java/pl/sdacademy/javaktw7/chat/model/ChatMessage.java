ce648c33164260fab8cf9b486e4353be3d7a85ee
zethrik <bartlomiej.jonda@gmail.com>
2018-09-29 10:50:35 +0200

    Added DatedChatMessage class

--- src/main/java/pl/sdacademy/javaktw7/chat/model/ChatMessage.java
+++ src/main/java/pl/sdacademy/javaktw7/chat/model/ChatMessage.java
@@ -3,12 +3,9 @@ package pl.sdacademy.javaktw7.chat.model;
 import java.io.Serializable;
 
 public class ChatMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String author;
     private String message;
-    public static final long serialVersionUID = 1L;
-
-    public ChatMessage() {
-    }
 
     public ChatMessage(String author, String message) {
         this.author = author;
